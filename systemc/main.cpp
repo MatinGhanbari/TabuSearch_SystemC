c08ea8dd603c188d541789c5744952454f52e64b
matinghanbari <ghanbarimatin6@gmail.com>
2023-01-27 02:49:46 +0330

    add index, weight, and counter vars for tabu_search

--- systemc/main.cpp
+++ systemc/main.cpp
@@ -5,14 +5,15 @@
 #include "monitor.cpp"
 
 int sc_main(int argc, char* argv[]) {
-    sc_signal<sc_uint<8>> n;
+    sc_signal<sc_uint<8>> n, W;
     sc_signal<sc_uint<9>> startNode;
 
-    n = 30;
+    n = 30, W = 20;
     startNode = 0b000000000;
     
     tabu_search ts("ts1");
     ts.n(n);
+    ts.W(W);
     ts.startNode(startNode);
 
     monitor monitor("monitor1");
--- systemc/tabu_search.h
+++ systemc/tabu_search.h
@@ -4,7 +4,16 @@
 SC_MODULE(tabu_search){
 
     sc_in<sc_uint<8>> n;
+    sc_in<sc_uint<8>> W;
     sc_in<sc_uint<9>> startNode;
+
+    sc_signal<sc_uint<9>> cNode;
+    sc_signal<sc_uint<9>> lNode;
+    sc_signal<sc_uint<9>> tempNode;
+
+    sc_uint<4> index = 0;
+    sc_uint<4> weight = 0;
+    sc_uint<4> counter = 0; // searches counter
 
     void search();
     void findNeighbors();
